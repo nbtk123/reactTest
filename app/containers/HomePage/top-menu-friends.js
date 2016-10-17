2bc7bc640e3ee7aa731907f5a7f9edd3f2c16550
Nir Barzilay <nbtk123@gmail.com>
2016-10-17 16:03:59 +0300

    Added top-menu-friends-list component

---
+++ app/containers/HomePage/top-menu-friends.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class TopMenuFriends extends Component {
+    render() {
+        return (
+            <div>
+                <img src="http://downloadicons.net/sites/default/files/msn-messenger-icon-48097.png" style={{width: '48px', height: '48px'}}/>
+                13
+            </div>
+        );
+    }
+}
+
+export default TopMenuFriends;
--- app/containers/HomePage/top-menu-right.js
+++ app/containers/HomePage/top-menu-right.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import TopMenuProfile from './top-menu-profile'
 import TopMenuSearch from './top-menu-search'
+import TopMenuFriends from './top-menu-friends'
 
 class TopMenuRight extends Component {
     render() {
         return (
             <ul>
                 <li style={{display: 'inline-block'}}><TopMenuProfile /></li>
+                <li style={{display: 'inline-block'}}><TopMenuFriends /></li>
                 <li style={{display: 'inline-block'}}><TopMenuSearch /></li>
             </ul>
         );
