62b0c8d6fb6b73de2e8707fc9c3b01381394f179
Nir Barzilay <nbtk123@gmail.com>
2016-10-17 15:53:58 +0300

    Moving TopMenuSearch styles to differnet file

--- app/containers/HomePage/top-menu-search.js
+++ app/containers/HomePage/top-menu-search.js
@@ -1,18 +1,12 @@
 import React, {Component} from 'react';
+import styles from './styles.css';
 
 class TopMenuSearch extends Component {
     render() {
         return (
             <div>
-                    <input type="textbox" style={{backgroundImage: 'url(http://icons.iconarchive.com/icons/graphicloads/100-flat/256/zoom-search-2-icon.png)', 
-                backgroundPosition: 'right top',
-                backgroundRepeat: 'no-repeat',
-                backgroundSize: 'contain',
-                backgroundColor: '#fff',
-                paddingRight: '48px',
-                height: '48px',
-                borderRadius: '25px',
-                border: '2px solid #73AD21'}}/>
+                <input  type="text"
+                        className={`${styles.topMenuSearch} ${styles.roundedCorners}`} />
             </div>
         );
     }
