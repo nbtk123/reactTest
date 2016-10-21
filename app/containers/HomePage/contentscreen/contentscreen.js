08a6ea0aac495b3ccca0b02de397548cf1b57da5
Nir Barzilay <nbtk123@gmail.com>
2016-10-21 14:07:21 +0300

    feature(homepage): set up level component

--- app/containers/HomePage/contentscreen/contentscreen.js
+++ app/containers/HomePage/contentscreen/contentscreen.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
-import AbilitiesComponent from './abilities-component/abilities-component.js'
+import AbilitiesComponent from './abilities-component/abilities-component'
+import LevelComponent from './level-component/level-component'
 
 class ContentScreen extends Component {
     render() {
         return (
-            <div>
-                <AbilitiesComponent />
+            <div style={{display: 'table', width: '100%'}}>
+                <div style={{display: 'table-row'}}>
+                    <div style={{display: 'table-cell'}}>
+                        <AbilitiesComponent />
+                    </div>
+                    <div style={{display: 'table-cell'}}>
+                        <LevelComponent />
+                    </div>
+                </div>
+                <div style={{display: 'table-row'}}>
+                    <div style={{display: 'table-cell'}}>
+                        BUY
+                    </div>
+                </div>
             </div>
         );
     }
