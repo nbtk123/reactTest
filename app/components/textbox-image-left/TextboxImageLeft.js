012cc5dafba1bf685c906a1f09a245905fb1f11a
Nir Barzilay <nbtk123@gmail.com>
2016-11-03 02:10:12 +0200

    feature(loginscreen): improve UI

--- app/components/textbox-image-left/TextboxImageLeft.js
+++ app/components/textbox-image-left/TextboxImageLeft.js
@@ -2,19 +2,31 @@ import React, {Component} from 'react';
 import styles from './styles.scss'; 
 
 class TextboxImageLeft extends Component {
+
+//backgroundImage: 'url(' + this.props.imgsrc + ')'
+
     render() {
         return (
-            <input type="text"
-                    className={styles.roundedTextbox}
-                    style={{backgroundImage: 'url(' + this.props.imgsrc + ')'}}
-                    placeholder={this.props.hint}/>
+            <div style={{marginBottom: '0.2vw'}}>
+                <div className={styles.shadow}
+                        style={{backgroundColor: this.props.shadowcolor}}>
+                </div>
+                <div className={styles.container}
+                        style={{backgroundColor: this.props.bgcolor, position: 'relative'}}>
+                    <input type="text"
+                            className={styles.roundedTextbox}
+                            placeholder={this.props.hint}/>
+                </div>
+            </div>
         );
     }
 }
 
 TextboxImageLeft.propTypes = {
     hint: React.PropTypes.string,
-    imgsrc: React.PropTypes.string
+    imgsrc: React.PropTypes.string,
+    bgcolor: React.PropTypes.string,
+    shadowcolor: React.PropTypes.string
 };
 
 export default TextboxImageLeft;
--- app/containers/LoginScreen/loginscreen.js
+++ app/containers/LoginScreen/loginscreen.js
@@ -5,33 +5,43 @@ import TextboxImageLeft from 'components/textbox-image-left/TextboxImageLeft.js'
 class LoginScreen extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className={['col-xs-8', 'col-xs-offset-2', styles.title].join(' ')}>
-                        Log in to<br/> your kingdom
-                    </div>
-                </div>
-                <div className="row">
-                    <div className={['col-xs-8', 'col-xs-offset-2'].join(' ')} style={{textAlign: 'center'}}>
-                        <TextboxImageLeft imgsrc="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/zoom-search-2-icon.png" hint="Username"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className={['col-xs-8', 'col-xs-offset-2'].join(' ')} style={{textAlign: 'center'}}>
-                        <TextboxImageLeft imgsrc="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/zoom-search-2-icon.png" hint="Password"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className={['col-xs-6'].join(' ')} style={{textAlign: 'left'}}>
-                        <div style={{fontSize: '0.7vw'}}>Forgot your password/username?</div>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-xs-2 col-xs-offset-4" style={{textAlign: 'center'}}>
-                        Sign in
-                    </div>
-                    <div className="col-xs-2" style={{textAlign: 'center'}}>
-                        Sign up
+            <div className={["container-fluid", styles.backgroundContainer].join(' ')}>
+                <div className="col-xs-8 col-xs-offset-2">
+                    <div className={["container-fluid", styles.contentContainer].join(' ')}>
+                        <div className="row">
+                            <div className={['col-xs-8', 'col-xs-offset-2', styles.title].join(' ')}>
+                                Log in to<br/> your kingdom
+                            </div>
+                        </div>
+                        <div className="row" style={{marginBottom: '0.7vw'}}>
+                            <div className={['col-xs-8 col-xs-offset-2'].join(' ')} style={{display: 'flex', justifyContent: 'center'}}>
+                                <TextboxImageLeft imgsrc="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/zoom-search-2-icon.png"
+                                                    hint="Username"
+                                                    bgcolor="#BABC4E"
+                                                    shadowcolor="#7F7317"/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className={['col-xs-8 col-xs-offset-2'].join(' ')} style={{display: 'flex', justifyContent: 'center'}}>
+                                <TextboxImageLeft imgsrc="http://icons.iconarchive.com/icons/graphicloads/100-flat/256/zoom-search-2-icon.png" 
+                                                    hint="Password"
+                                                    bgcolor="#BABC4E"
+                                                    shadowcolor="#7F7317"/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className={['col-xs-3 col-xs-offset-5'].join(' ')} style={{textAlign: 'right'}}>
+                                <div style={{fontSize: '0.7vw'}}>Forgot your password/username?</div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-2 col-xs-offset-4" style={{textAlign: 'center'}}>
+                                Sign in
+                            </div>
+                            <div className="col-xs-2" style={{textAlign: 'center'}}>
+                                Sign up
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
